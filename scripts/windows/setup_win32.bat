6fe4bcdce5f1a9bb98342ccbf74a71acca963464
jbreizh <jb.lecoz@gmail.com>
2013-07-24 00:13:16 +1100

    Changement pour la création des paquet

--- scripts/windows/setup_win32.bat
+++ scripts/windows/setup_win32.bat
@@ -1,4 +1,5 @@
 cd ..
 cd ..
+cd src
 C:\Python27\python.exe setup_win32.py innosetup
 pause
--- src/setup_win32.py
+++ src/setup_win32.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-import py2exe, glob, os, innosetup
+from setuptools import setup, find_packages
+import py2exe, innosetup
 from actimaths.values import VERSION, DESCRIPTION, LICENSE, COPYRIGHTS, MAIL, WEBSITE
 
 setup_iss = '''
@@ -26,35 +26,19 @@ setup(name = "Actimaths",
     author = COPYRIGHTS,
     author_email = MAIL,
     url = WEBSITE,
-    package_dir={'actimaths':'actimaths'},
-    packages=['actimaths','actimaths.exercices_actimaths','actimaths.exercices_actimaths.outils','actimaths.exercices_actimaths.classes','actimaths.exercices_pyromaths','actimaths.exercices_pyromaths.outils','actimaths.exercices_pyromaths.classes'],
-    data_files=[
-        (r'data/images', [r'data/images/actimaths.ico',r'data/images/actimaths.png', r'data/images/whatsthis.png']),
-        (r'data/images/vignettes', glob.glob(r'data/images/vignettes/*.png')),
-        (r'data/packages', glob.glob(r'data/packages/*')),  
-        (r'data/actimaths/modeles/page/BiColonneIdentique', glob.glob(r'data/actimaths/modeles/page/BiColonneIdentique/*.tex')),
-        (r'data/actimaths/modeles/page/BiColonneInverse', glob.glob(r'data/actimaths/modeles/page/BiColonneInverse/*.tex')),
-        (r'data/actimaths/modeles/page/MonoColonne', glob.glob(r'data/actimaths/modeles/page/MonoColonne/*.tex')),
-        (r'data/actimaths/modeles/page/Test', glob.glob(r'data/actimaths/modeles/page/Test/*.tex')),
-        (r'data/actimaths/modeles/presentation/BiColonneIdentique', glob.glob(r'data/actimaths/modeles/presentation/BiColonneIdentique/*.tex')),
-        (r'data/actimaths/modeles/presentation/BiColonneIdentiqueCompteur', glob.glob(r'data/actimaths/modeles/presentation/BiColonneIdentiqueCompteur/*.tex')),
-        (r'data/actimaths/modeles/presentation/BiColonneInverse', glob.glob(r'data/actimaths/modeles/presentation/BiColonneInverse/*.tex')),
-        (r'data/actimaths/modeles/presentation/BiColonneInverseCompteur', glob.glob(r'data/actimaths/modeles/presentation/BiColonneInverseCompteur/*.tex')),
-        (r'data/actimaths/modeles/presentation/MonoColonne', glob.glob(r'data/actimaths/modeles/presentation/MonoColonne/*.tex')),
-        (r'data/actimaths/modeles/presentation/MonoColonneCompteur', glob.glob(r'data/actimaths/modeles/presentation/MonoColonneCompteur/*.tex')),
-        (r'data/actimaths/onglets', glob.glob(r'data/actimaths/onglets/*.xml')),
-        (r'data/pyromaths/modeles/page/TriColonne', glob.glob(r'data/pyromaths/modeles/page/TriColonne/*.tex')),
-        (r'data/pyromaths/modeles/page/BiColonne', glob.glob(r'data/pyromaths/modeles/page/BiColonne/*.tex')),
-        (r'data/pyromaths/modeles/page/MonoColonne', glob.glob(r'data/pyromaths/modeles/page/MonoColonne/*.tex')),
-        (r'data/pyromaths/modeles/presentation/Vignette',glob.glob(r'data/pyromaths/modeles/presentation/Vignette/*.tex')),
-        (r'data/pyromaths/onglets', glob.glob(r'data/pyromaths/onglets/*.xml')),
-        ],
-
+    packages=['actimaths',
+              'actimaths.exercices_actimaths','actimaths.exercices_actimaths.outils',
+              'actimaths.exercices_pyromaths','actimaths.exercices_pyromaths.outils'],
+    package_data={'actimaths': [r'images/*'],
+                  'actimaths.exercices_actimaths': [r'modeles/*/*/*', r'onglets/*', r'packages/*', r'vignettes/*'],
+                  'actimaths.exercices_pyromaths': [r'modeles/*/*/*', r'onglets/*', r'packages/*', r'vignettes/*']},
+    data_files=[(r'data/images', [r'data/images/actimaths.ico'])],   
     platforms = ['windows'],
     options =
     {
         'py2exe':
         {
+            "skip_archive": True,
             "compressed": 1, "optimize": 2, "bundle_files": 3,
             "includes":["sip", "gzip"]
         },
