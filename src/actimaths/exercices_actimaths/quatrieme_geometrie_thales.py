4c832d666e89b774e3a9c0bd4e5d2f4c4f246b25
jb <jbreizh@localhost.localdomain>
2015-06-05 20:19:22 +1100

    Amélioration de thales en quatrieme

--- src/actimaths/exercices_actimaths/quatrieme_geometrie_thales.py
+++ src/actimaths/exercices_actimaths/quatrieme_geometrie_thales.py
@@ -40,20 +40,6 @@ def caracteristique_sommet_thales():
     coordonnee = (random.uniform(-2,2),random.uniform(2,4),random.uniform(-4,-2),random.uniform(-4,-2),random.uniform(2,4),random.uniform(-4,-2))
     return (nom_ordre,nom_melange,coordonnee)
 
-def tex_figure_thales(enonce,nom_ordre,nom_melange,coordonnee):
-    enonce.append("\\begin{center}")
-    enonce.append("\\psset{unit=0.5cm}")
-    enonce.append('\\begin{pspicture*}(-5,-5)(5,5)')
-    enonce.append("\\pstGeonode(%s,%s){%s}(%s,%s){%s}(%s,%s){%s}" %(coordonnee[0],coordonnee[1],nom_melange[0],coordonnee[2],coordonnee[3],nom_melange[1],coordonnee[4],coordonnee[5],nom_melange[2]))
-    enonce.append("\\pstMiddleAB{%s}{%s}{%s}" %(nom_ordre[0],nom_ordre[1],nom_ordre[3]))
-    enonce.append("\\pstMiddleAB{%s}{%s}{%s}" %(nom_ordre[0],nom_ordre[2],nom_ordre[4]))
-    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[0],nom_ordre[1]))
-    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[0],nom_ordre[2]))
-    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[1],nom_ordre[2]))
-    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[3],nom_ordre[4]))
-    enonce.append('\\end{pspicture*}')
-    enonce.append("\\end{center}")
-
 def caracteristique_cote_thales(longueur_min,longueur_max,droite_milieu=False):
     ## longueur des cotés du petit triangle
     mesure = [random.randint(int(sqrt(longueur_min)),int(sqrt(longueur_max))) for i in xrange(3)]
@@ -66,12 +52,12 @@ def caracteristique_cote_thales(longueur_min,longueur_max,droite_milieu=False):
     for i in range(3):
         mesure.append(mesure[i]*coef_agrandissement)
     ## choix de la mesure manquante
-    manque = random.randrange(6)
+    mesure_manquante = random.randrange(6)
     ## Tex des mesures pour le sujet et le corrigé
     tex_mesure_sujet =[]
     tex_mesure_corrige =[]
     for i in range(6):
-        if i != manque:
+        if i != mesure_manquante:
             tex_mesure_sujet.append("%s" % mesure[i])
             tex_mesure_corrige.append("%s" % mesure[i])
         else:
@@ -79,6 +65,25 @@ def caracteristique_cote_thales(longueur_min,longueur_max,droite_milieu=False):
             tex_mesure_corrige.append("\\boxed{%s}" % mesure[i])
 
     return (tex_mesure_sujet,tex_mesure_corrige)
+
+def tex_figure_thales(enonce,nom_ordre,nom_melange,coordonnee,coefficient):
+    enonce.append("\\begin{center}")
+    enonce.append("\\psset{unit=0.5cm}")
+    enonce.append('\\begin{pspicture*}(-5,-5)(5,5)')
+    # points du grand triangle
+    enonce.append("\\pstGeonode(%s,%s){%s}(%s,%s){%s}(%s,%s){%s}" %(coordonnee[0],coordonnee[1],nom_melange[0],coordonnee[2],coordonnee[3],nom_melange[1],coordonnee[4],coordonnee[5],nom_melange[2]))
+    # points du petit triangle
+    enonce.append("\\pstHomO[HomCoef=%s]{%s}{%s,%s}[%s,%s]" %(coefficient,nom_ordre[0],nom_ordre[1],nom_ordre[2],nom_ordre[3],nom_ordre[4]))
+    # grand triangle
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[1],nom_ordre[0]))
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[2],nom_ordre[1]))
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[0],nom_ordre[2]))
+    # petit triangle
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[3],nom_ordre[0]))
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[4],nom_ordre[3]))
+    enonce.append("\\pstLineAB{%s}{%s}" %(nom_ordre[0],nom_ordre[4]))
+    enonce.append('\\end{pspicture*}')
+    enonce.append("\\end{center}")
 
 def tex_figure_thales_mesure(enonce,nom_ordre,nom_melange,coordonnee,tex_mesure):
     enonce.append("\\begin{center}")
@@ -113,6 +118,12 @@ def DroiteMilieu(parametre):
     tex_figure_thales_mesure(exo,nom_ordre,nom_melange,coordonnee,tex_mesure_sujet)
     tex_figure_thales_mesure(cor,nom_ordre,nom_melange,coordonnee,tex_mesure_corrige)
     cor.append(u"On utilise la \\textbf{Propriété des Milieux}")
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    cor.append("\\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} \\\\" %(tex_mesure_sujet[0],tex_mesure_sujet[3],tex_mesure_sujet[1],tex_mesure_sujet[4],tex_mesure_sujet[2],tex_mesure_sujet[5]))
+    cor.append("\\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} \\\\" %(tex_mesure_corrige[0],tex_mesure_corrige[3],tex_mesure_corrige[1],tex_mesure_corrige[4],tex_mesure_corrige[2],tex_mesure_corrige[5]))
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
     return (exo, cor, question)
 
 def ThalesQuotient(parametre):
@@ -122,9 +133,10 @@ def ThalesQuotient(parametre):
     cor = [ ]
     ## ---Calcul des paramètres---
     (nom_ordre,nom_melange,coordonnee) = caracteristique_sommet_thales()
+    coefficient = random.uniform(0.25,0.75)
     ## ---Redaction---
-    tex_figure_thales(exo,nom_ordre,nom_melange,coordonnee)
-    tex_figure_thales(cor,nom_ordre,nom_melange,coordonnee)
+    tex_figure_thales(exo,nom_ordre,nom_melange,coordonnee,coefficient)
+    tex_figure_thales(cor,nom_ordre,nom_melange,coordonnee,coefficient)
     exo.append("\\begin{center}")
     cor.append("\\begin{center}")
     exo.append("$\\dfrac{\\ldots}{\\ldots}=\\dfrac{\\ldots}{\\ldots}=\\dfrac{\\ldots}{\\ldots}$")
@@ -140,9 +152,10 @@ def ThalesTableau(parametre):
     cor = [ ]
     ## ---Calcul des paramètres---
     (nom_ordre,nom_melange,coordonnee) = caracteristique_sommet_thales()
+    coefficient = random.uniform(0.25,0.75)
     ## ---Redaction---
-    tex_figure_thales(exo,nom_ordre,nom_melange,coordonnee)
-    tex_figure_thales(cor,nom_ordre,nom_melange,coordonnee)
+    tex_figure_thales(exo,nom_ordre,nom_melange,coordonnee,coefficient)
+    tex_figure_thales(cor,nom_ordre,nom_melange,coordonnee,coefficient)
     exo.append("\\begin{center}")
     cor.append("\\begin{center}")
     exo.append("\\begin{tabular}{|l|c|c|c|}")
@@ -171,8 +184,15 @@ def ThalesQuotientMesure(parametre):
     ## ---Calcul des paramètres---
     (nom_ordre,nom_melange,coordonnee) = caracteristique_sommet_thales()
     (tex_mesure_sujet,tex_mesure_corrige) = caracteristique_cote_thales(parametre[0],parametre[1])
-    ## ---Redaction---
+    ## ---Redaction du sujet---
     tex_figure_thales_mesure(exo,nom_ordre,nom_melange,coordonnee,tex_mesure_sujet)
+    ## ---Redaction du corrige---
     tex_figure_thales_mesure(cor,nom_ordre,nom_melange,coordonnee,tex_mesure_corrige)
     cor.append(u"On utilise le \\textbf{Théorème de Thalès}")
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    cor.append("\\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} \\\\" %(tex_mesure_sujet[0],tex_mesure_sujet[3],tex_mesure_sujet[1],tex_mesure_sujet[4],tex_mesure_sujet[2],tex_mesure_sujet[5]))
+    cor.append("\\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} & = & \\dfrac{%s}{%s} \\\\" %(tex_mesure_corrige[0],tex_mesure_corrige[3],tex_mesure_corrige[1],tex_mesure_corrige[4],tex_mesure_corrige[2],tex_mesure_corrige[5]))
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
     return (exo, cor, question)
