d72c477502d67a5d727cf2445721e0eab7f92c7e
jb <jbreizh@localhost.localdomain>
2015-07-01 20:59:25 +1100

    Exercice sur les parallèles et perpendiculaire en 6eme

--- src/actimaths/exercices_actimaths/sixieme_geometrie_droiteDemidroiteSegment.py
+++ src/actimaths/exercices_actimaths/sixieme_geometrie_droiteDemidroiteSegment.py
@@ -85,14 +85,14 @@ def tex_perpendiculaire(enonce,angle,corrige):
     enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,1){B}")
     # droite de depart
     enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{A}{B}[C]" %angle[0])
-    enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6]{A}{C}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{A}{C}")
     # droite "perpendiculaire"
     enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{A}{C}[D]" %angle[1])
-    enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6]{A}{D}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{A}{D}")
     # Affichage du corrige
     if corrige:
         enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{A}{C}[E]")
-        enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6,linecolor=red]{A}{E}")
+        enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6,linecolor=red]{A}{E}")
         enonce.append("\\pstRightAngle[linecolor=red]{C}{A}{E}")
 
     enonce.append("\\end{pspicture*}")
@@ -112,13 +112,71 @@ def tex_parallele(enonce,angle,corrige):
 
     enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{C}{D}[E]")
     enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{D}{C}[F]" %angle[1])
-
-    enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6]{C}{E}")
-    enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6]{D}{F}")
+    #tracer des droites
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{C}{E}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{D}{F}")
     # Affichage du corrige
     if corrige:
         enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{D}{C}[G]")
         enonce.append("\\pstLineAB[nodesepA=-6, nodesepB=-6,linecolor=red]{D}{G}")
+    enonce.append("\\end{pspicture*}")
+    enonce.append("\\end{center}")
+
+def tex_parallele_propriete(enonce,angle,angle_droit):
+    enonce.append("\\begin{center}")
+    enonce.append("\\psset{unit=0.5cm}")
+    enonce.append("\\begin{pspicture*}(-5,-5)(5,5)")
+    # point statique
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,0){O}")
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,-1){A}")
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,1){B}")
+
+    enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{O}{A}[C]" %angle)
+    enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{O}{B}[D]" %angle)
+
+    enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{C}{D}[E]")
+    enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{D}{C}[F]")
+    #tracer des droites
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{C}{D}")
+    enonce.append(" \\naput[npos=0.8]{$(d_3)$}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{C}{E}")
+    enonce.append(" \\nbput[npos=0.75]{$(d_1)$}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{D}{F}")
+    enonce.append(" \\nbput[npos=0.75]{$(d_2)$}")
+    #tracer des codes d'angles droits
+    if angle_droit[0]:
+        enonce.append("\\pstRightAngle{D}{C}{E}")
+    if angle_droit[1]:
+        enonce.append("\\pstRightAngle{C}{D}{F}")
+    enonce.append("\\end{pspicture*}")
+    enonce.append("\\end{center}")
+
+def tex_perpendiculaire_propriete(enonce,angle,angle_droit):
+    enonce.append("\\begin{center}")
+    enonce.append("\\psset{unit=0.5cm}")
+    enonce.append("\\begin{pspicture*}(-5,-5)(5,5)")
+    # point statique
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,0){O}")
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,-1){A}")
+    enonce.append("\\pstGeonode[PointSymbol=none,PointName=none](0,1){B}")
+
+    enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{O}{A}[C]" %angle)
+    enonce.append("\\pstRotation[RotAngle=%s,PointSymbol=none,PointName=none]{O}{B}[D]" %angle)
+
+    enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{C}{D}[E]")
+    enonce.append("\\pstRotation[RotAngle=90,PointSymbol=none,PointName=none]{D}{C}[F]")
+    #tracer des droites
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{C}{D}")
+    enonce.append(" \\naput[npos=0.8]{$(d_3)$}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{C}{E}")
+    enonce.append(" \\nbput[npos=0.75]{$(d_1)$}")
+    enonce.append("\\pstLineAB[nodesepA=-6,nodesepB=-6]{D}{F}")
+    enonce.append(" \\nbput[npos=0.75]{$(d_2)$}")
+    #tracer des codes d'angles droits
+    if angle_droit[0]:
+        enonce.append("\\rput*(-3,4){\\psframebox[linewidth=0.04,fillstyle=solid]{$(d_1)//(d_2)$}}")
+    if angle_droit[1]:
+        enonce.append("\\pstRightAngle{C}{D}{F}")
     enonce.append("\\end{pspicture*}")
     enonce.append("\\end{center}")
 
@@ -202,7 +260,7 @@ def Appartient(parametre):
 
 def Perpendiculaire(parametre):
     ## ---Initialisation---
-    question = u"Les deux droites sont elles perpendiculaires :"
+    question = u"Les droites sont elles perpendiculaires :"
     exo = []
     cor = []
     ## ---Calcul des paramètres---
@@ -225,7 +283,7 @@ def Perpendiculaire(parametre):
 
 def Parallele(parametre):
     ## ---Initialisation---
-    question = u"Les deux droites sont elles parallèles :"
+    question = u"Les droites sont elles parallèles :"
     exo = []
     cor = []
     ## ---Calcul des paramètres---
@@ -241,6 +299,62 @@ def Parallele(parametre):
     ## ---Redaction---
     tex_parallele(exo, angle, False)
     tex_parallele(cor, angle, True)
+    cor.append("\\begin{center}")
+    cor.append("\\fbox{%s}" %reponse)
+    cor.append("\\end{center}")
+    return (exo, cor, question)
+
+def ParallelePropriete(parametre):
+    ## ---Initialisation---
+    question = u"$(d_1)$ et $(d_2)$ sont elles parallèles :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    angle = random.randrange(90)
+    parallele = random.randrange(2)
+    if parallele:
+        angle_droit = [ True , True ]
+        reponse = u"$(d_1)$ et $(d_2)$ sont parallèles"
+    else:
+        angle_droit_manquant = random.randrange(3)
+        if angle_droit_manquant == 0:
+            angle_droit = [ True , False ]  
+        elif angle_droit_manquant == 1:
+            angle_droit = [ False , True ]  
+        else:
+            angle_droit = [ False , False ]  
+        reponse = u"$(d_1)$ et $(d_2)$ ne sont pas parallèles"
+    ## ---Redaction---
+    tex_parallele_propriete(exo, angle, angle_droit)
+    tex_parallele_propriete(cor, angle, angle_droit)
+    cor.append("\\begin{center}")
+    cor.append("\\fbox{%s}" %reponse)
+    cor.append("\\end{center}")
+    return (exo, cor, question)
+
+def PerpendiculairePropriete(parametre):
+    ## ---Initialisation---
+    question = u"$(d_2)$ et $(d_3)$ sont elles perpendiculaires :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    angle = random.randrange(90)
+    perpendiculaire = random.randrange(2)
+    if perpendiculaire:
+        angle_droit = [ True , True ]
+        reponse = u"$(d_2)$ et $(d_3)$ sont perpendiculaires"
+    else:
+        angle_droit_manquant = random.randrange(3)
+        if angle_droit_manquant == 0:
+            angle_droit = [ True , False ]  
+        elif angle_droit_manquant == 1:
+            angle_droit = [ False , True ]  
+        else:
+            angle_droit = [ False , False ]  
+        reponse = u"$(d_2)$ et $(d_3)$ ne sont pas perpendiculaires"
+    ## ---Redaction---
+    tex_perpendiculaire_propriete(exo, angle, angle_droit)
+    tex_perpendiculaire_propriete(cor, angle, angle_droit)
     cor.append("\\begin{center}")
     cor.append("\\fbox{%s}" %reponse)
     cor.append("\\end{center}")
