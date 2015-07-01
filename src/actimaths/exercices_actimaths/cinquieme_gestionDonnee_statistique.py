bc7d949c24b9767a8aecff243b0c7ac4aacd5839
jb <jbreizh@localhost.localdomain>
2015-07-01 20:58:58 +1100

    Exercice de statistique en 5eme

--- src/actimaths/exercices_actimaths/cinquieme_gestionDonnee_statistique.py
+++ src/actimaths/exercices_actimaths/cinquieme_gestionDonnee_statistique.py
@@ -22,83 +22,227 @@
 #
 
 import random
+import copy
 
-def tex_tableau(texte, nombre):
-    texte.append("\\begin{center}")
-    texte.append("\\begin{tabular}{|c|c|c|c|}")
-    texte.append("\\hline")
-    texte.append("%s & %s & %s & %s \\\\" % (nombre[0][0], nombre[1][0], nombre[2][0], nombre[3][0]))
-    texte.append("\\hline")
-    texte.append("%s & %s & %s & %s \\\\" % (nombre[0][1], nombre[1][1], nombre[2][1], nombre[3][1]))
-    texte.append("\\hline")   
-    texte.append("\\end{tabular}")
-    texte.append("\\end{center}")
+#---------------methode--------------------------------
+def creation_tableau(parametre,structure):
+    ## ---Calcul des paramètres---
+    nombreColonne = random.randrange(3,5)
+    #caractere
+    caractere = [u"Caractère"]
+    caractereTemp = random.sample(range(parametre[0],parametre[1]), nombreColonne)
+    caractereTemp.sort()
+    caractere.extend(caractereTemp)
+    caractere.append("Total")
+    #effectif
+    effectif = ["Effectif"]
+    effectifTemp = random.sample(range(parametre[0],parametre[1]), nombreColonne)
+    effectifTotal = 0
+    for i in range(len(effectifTemp)):
+        effectifTotal += effectifTemp[i]
+    effectif.extend(effectifTemp)
+    effectif.append(effectifTotal)
+    #frequence
+    frequence = [u"Fréquence"]
+    frequenceTemp = [round(float(i)/effectifTotal,2) for i in effectifTemp]
+    frequence.extend(frequenceTemp)
+    frequence.append(1)
+    #angle
+    angle = ["Angle"]
+    angleTemp = [round(float(i)/effectifTotal*360,1) for i in effectifTemp]
+    angle.extend(angleTemp)
+    angle.append(360)
+    ## ---remplissage du tableau---
+    tableau = []
+    for i in range(len(caractere)):
+        ligne = []
+        if structure[0]:
+            ligne.append(caractere[i])
+        if structure[1]:
+            ligne.append(effectif[i])
+        if structure[2]:
+            ligne.append(frequence[i])
+        if structure[3]:
+            ligne.append(angle[i])
+        tableau.append(ligne)
+    return tableau
 
-def tex_calcul_coefficient(exo,cor):
-    nombre = random.sample(range(1,11), 4)
-    coefficient = random.randrange (2, 10)
-    colonneCor = random.randrange (0, 4)
-    nombreExo = []
-    nombreCor = []
-    for i in range(len(nombre)):
-        nombreExo.append([nombre[i],nombre[i]*coefficient])
-        if i == colonneCor:
-            nombreCor.append(["\\textbf{%s}" % nombre[i],"\\textbf{%s}" % (nombre[i]*coefficient)])
+def tex_tableau(tex, contenu):
+    nombreColonne = len(contenu)
+    if nombreColonne != 0:
+        nombreLigne = len(contenu[0])
+        # entete du tableau
+        tex.append("\\begin{tiny}")
+        tex.append("\\begin{center}")
+        ligne = "\\begin{tabular}{|"
+        for i in range(len(contenu)):
+            ligne += "c|"
+        ligne += "}"
+        tex.append(ligne)
+        # corps du tableau
+        for i in range(nombreLigne):
+            tex.append("\\hline")
+            ligne = ""
+            for j in range(nombreColonne):
+                ligne += "%s" % contenu[j][i]
+                if j != nombreColonne - 1:
+                   ligne += "&"
+            ligne += "\\\\"
+            tex.append(ligne)
+        tex.append("\\hline")
+        # fin du tableau
+        tex.append("\\end{tabular}")
+        tex.append("\\end{center}")
+        tex.append("\\end{tiny}")
+
+#---------------Construction--------------------------------
+def CalculEffectifSerie(parametre):
+    ## ---Initialisation---
+    question = u"Compléter le tableau :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    tableau = creation_tableau(parametre,[True,True,False,False])
+    manquant = random.randrange(1,len(tableau)-1)
+    ## ---serie brute---
+    serie = []
+    for i in range(1,len(tableau) - 1):
+        for j in range(tableau[i][1]):
+            serie.append(tableau[i][0])
+    ## ---serie pour le sujet---
+    serieSujet = copy.deepcopy(serie)
+    random.shuffle(serieSujet)
+    #tableau pour le sujet
+    tableauSujet = copy.deepcopy(tableau)
+    tableauSujet[manquant][1] = "$\\ldots$"
+    ## ---Redaction---
+    exo.append(u"Série statistique : \\newline")
+    cor.append(u"Série statistique : \\newline")
+    exo.append("\\begin{center}")
+    cor.append("\\begin{center}")
+    ligne = ""
+    for i in range(len(serieSujet)):
+        ligne += "%s" %serieSujet[i]
+        if i != len(serieSujet) - 1:
+            ligne += " ; "
+    exo.append(ligne)
+    cor.append(ligne)
+    exo.append("\\end{center}")
+    cor.append("\\end{center}")
+    exo.append(u"Tableau caractère - effectif :")
+    cor.append(u"Tableau caractère - effectif :")
+    tex_tableau(exo, tableauSujet)
+    tex_tableau(cor, tableauSujet)
+    cor.append(u"On compte un effectif de \\fbox{%s} pour le caractère %s dans la série." %(tableau[manquant][1],tableau[manquant][0]))
+    return (exo, cor, question)
+
+def CalculEffectifTableau(parametre):
+    ## ---Initialisation---
+    question = u"Calculer l'effectif manquant :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    tableau = creation_tableau(parametre, [True,True,False,False])
+    effectifTotal = tableau[len(tableau)-1][1]
+    manquant = random.randrange(1,len(tableau)-1)
+    #tableau pour le sujet
+    tableauSujet = copy.deepcopy(tableau)
+    tableauSujet[manquant][1] = "$\\ldots$"
+    # somme des effectifs
+    somme = 0
+    for i in range(1, len(tableau)-1):
+        if i != manquant:
+            somme += tableau[i][1]
+    ## ---Redaction---
+    tex_tableau(exo, tableauSujet)
+    tex_tableau(cor, tableauSujet)
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    ligne1 = ""
+    for i in range(1, len(tableau)-1):
+        if i == manquant:
+            ligne1 += "\\ldots"
         else:
-            nombreCor.append([nombre[i],nombre[i]*coefficient])
-     
-    tex_tableau(exo, nombreExo)
-    tex_tableau(cor, nombreCor)
-    cor.append("$$ %s \\times \\ldots = %s $$" % (nombreExo[colonneCor][0], nombreExo[colonneCor][1]))    
-    cor.append("$$ %s \\times \\textbf{%s} = %s $$" % (nombreExo[colonneCor][0], coefficient, nombreExo[colonneCor][1]))
-    cor.append("Le coefficient est \\textbf{%s}" % coefficient)
+            ligne1 += "%s" % tableau[i][1]
+        if i != len(tableau)-2:
+            ligne1 += "+"
+    ligne1 += "& = & %s \\\\" % effectifTotal
+    cor.append(ligne1)
+    cor.append("\\ldots + %s & = & %s \\\\" %(somme, effectifTotal))
+    cor.append("\\ldots & = & %s - %s \\\\" %(effectifTotal, somme))
+    cor.append("\\ldots & = & \\boxed{%s} \\\\" %(effectifTotal- somme))
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
+    return (exo, cor, question)
 
-def tex_complete_tableau(exo,cor):
-    nombre = random.sample(range(1,11), 4)
-    coefficient = random.randrange (2, 10)
-    colonneExo = random.randrange (0, len(nombre))
-    ligneExo = random.randrange (0, 2)
-    nombreExo = []
-    nombreCor = []
-    for i in range(len(nombre)):
-        if i == colonneExo:
-            if ligneExo == 0:
-                 nombreExo.append(["\\ldots",nombre[i]*coefficient])
-                 nombreCor.append(["\\textbf{%s}" % nombre[i],nombre[i]*coefficient])
-            else:
-                nombreExo.append([nombre[i],"\\ldots"])
-                nombreCor.append([nombre[i],"\\textbf{%s}" % (nombre[i]*coefficient)])
-                print nombre[i]*coefficient
+def CalculEffectifTotal(parametre):
+     ## ---Initialisation---
+    question = u"Calculer l'effectif total :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    tableau = creation_tableau(parametre,[True,True,False,False])
+    #tableau pour le sujet
+    tableauSujet = copy.deepcopy(tableau)
+    tableauSujet[len(tableau)-1][1] = "$\\ldots$"
+    ## ---Redaction---
+    tex_tableau(exo, tableauSujet)
+    tex_tableau(cor, tableauSujet)
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    ligne1 = "\\ldots & = & "
+    for i in range(1, len(tableau)-1):
+        ligne1 += "%s" % tableau[i][1]
+        if i != len(tableau)-2:
+            ligne1 += "+"
         else:
-            nombreExo.append([nombre[i],nombre[i]*coefficient])
-            nombreCor.append([nombre[i],nombre[i]*coefficient])
-        
-    tex_tableau(exo, nombreExo)
-    tex_tableau(cor, nombreCor)
-    cor.append("Le coefficient est %s donc" % coefficient)
-    cor.append("$$ %s \\times %s = %s $$" % (nombreExo[colonneExo][0], coefficient, nombreExo[colonneExo][1]))    
-    cor.append("$$ %s \\times %s = %s $$" % (nombreCor[colonneExo][0], coefficient, nombreCor[colonneExo][1]))
-    cor.append("Le nombre est %s" % nombreCor[colonneExo][ligneExo])
-
-def CalculEffectif(parametre):
-    exo = [ u"\\question{Calcul le coefficient de proportionnalité du tableau}"]
-    cor = [ u"\\corrige{Calcul le coefficient de proportionnalité du tableau}"]
-    for i in range(2):
-	exo.extend(["\\begin{column}{.5\\textwidth}"])
-        cor.extend(["\\begin{column}{.5\\textwidth}"])
-
-        exo.extend(["\\end{column}"])
-        cor.extend(["\\end{column}"])
-    return (exo, cor)
+            ligne1 += "\\\\"
+    cor.append(ligne1)
+    cor.append("\\ldots & = & \\boxed{%s} \\\\" %tableau[len(tableau)-1][1])
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
+    return (exo, cor, question)
 
 def CalculFrequence(parametre):
-    exo = [ u"\\question{Calcul le coefficient de proportionnalité du tableau}"]
-    cor = [ u"\\corrige{Calcul le coefficient de proportionnalité du tableau}"]
-    for i in range(2):
-	exo.extend(["\\begin{column}{.5\\textwidth}"])
-        cor.extend(["\\begin{column}{.5\\textwidth}"])
+     ## ---Initialisation---
+    question = u"Calculer la frequence manquante :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    tableau = creation_tableau(parametre,[True,True,True,False])
+    manquant = random.randrange(1,len(tableau)-1)
+    #tableau pour le sujet
+    tableauSujet = copy.deepcopy(tableau)
+    tableauSujet[manquant][2] = "$\\ldots$"
+    ## ---Redaction---
+    tex_tableau(exo, tableauSujet)
+    tex_tableau(cor, tableauSujet)
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    cor.append("\\ldots & = & %s \\div %s \\\\" %(tableau[manquant][1],tableau[len(tableau)-1][1]))
+    cor.append("\\ldots & = & \\boxed{%s} \\\\" %tableau[manquant][2])
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
+    return (exo, cor, question)
 
-        exo.extend(["\\end{column}"])
-        cor.extend(["\\end{column}"])
-    return (exo, cor)
-
+def CalculAngleDiagramme(parametre):
+     ## ---Initialisation---
+    question = u"Calculer l'angle manquant :"
+    exo = []
+    cor = []
+    ## ---Calcul des paramètres---
+    tableau = creation_tableau(parametre,[True,True,False,True])
+    manquant = random.randrange(1,len(tableau)-1)
+    #tableau pour le sujet
+    tableauSujet = copy.deepcopy(tableau)
+    tableauSujet[manquant][2] = "$\\ldots$"
+    ## ---Redaction---
+    tex_tableau(exo, tableauSujet)
+    tex_tableau(cor, tableauSujet)
+    cor.append("\\begin{center}")
+    cor.append("$\\begin{aligned}")
+    cor.append("\\ldots & = & %s \\div %s \\times 360 \\\\" %(tableau[manquant][1],tableau[len(tableau)-1][1]))
+    cor.append("\\ldots & = & \\boxed{%s} \\\\" %tableau[manquant][2])
+    cor.append("\\end{aligned}$")
+    cor.append("\\end{center}")
+    return (exo, cor, question)
